168dd04d0742a4a396bf196b29df341853a8458d
Calvin H <hanisch.calvin@gmail.com>
2025-09-22 23:29:51 +0200

    Header for Desktop and Mobile, padding variables for main content, wrapped content in <main> (#13)
    * header implemented and variables for paddings, also wrapped the router in a main
    
    * padding-top for mobile version on main element
    
    * removed sidebar component from app.html, since its not used

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from "@angular/core";
+import { Contact } from "app/core/interfaces/contact";
 
 @Component({
-  selector: 'app-header',
-  imports: [],
-  templateUrl: './header.html',
-  styleUrl: './header.scss'
+	selector: "app-header",
+	imports: [],
+	templateUrl: "./header.html",
+	styleUrl: "./header.scss",
 })
 export class Header {
-
+	profile = input<Contact>();
 }
