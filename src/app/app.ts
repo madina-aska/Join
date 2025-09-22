58348f0553636a69dc33b154b5eb47f053a7b8bc
Falko Katzer <162439151+hcplayer1988@users.noreply.github.com>
2025-09-22 15:16:21 +0200

    Falko (#8)
    * Added base structure of the project + CSS Colors as global variables
    
    Added components, services and CSS for base structure of the project.
    Also implemented the base app.html structure.
    
    * Add typography documentation and local Inter font configuration
    
    - added readme per section for requirements
    - added global font sizes!
    - Read4Main Merg!
    
    Co-Authored-By: Calvin H <213484571+CalvinH-Dev@users.noreply.github.com>
    
    * Changed the Fonts
    
    - removed static Fonts
    - added variableFonts
    
    * Changed Fonts
    
    Chnaged ttf into woff2
    
    * font-face, variables & imports of sccs-files
    
    * basic build
    
    * design add Contact
    
    * updated font-family
    
    * design mobile finished
    
    * start with desktop design
    
    * desktop design continued
    
    * desktop design continued 600px
    
    * variable status false
    
    * delete readme
    
    ---------
    
    Co-authored-by: CalvinH-Dev <hanisch.calvin@gmail.com>
    Co-authored-by: Slin <phillips96.business@gmail.com>
    Co-authored-by: Calvin H <213484571+CalvinH-Dev@users.noreply.github.com>

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from "@angular/router";
 import { Footer } from "./shared/footer/footer";
 import { Header } from "./shared/header/header";
 import { Sidebar } from "./shared/sidebar/sidebar";
+import { AddContact } from "./main/contacts/add-contact/add-contact";
 
 @Component({
 	selector: "app-root",
-	imports: [RouterOutlet, Sidebar, Header, Footer],
+	imports: [RouterOutlet, Sidebar, Header, Footer, AddContact],
 	templateUrl: "./app.html",
 	styleUrl: "./app.scss",
 })
--- src/app/main/contacts/add-contact/add-contact.ts
+++ src/app/main/contacts/add-contact/add-contact.ts
@@ -1,11 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-add-contact',
-  imports: [],
+  imports: [FormsModule, RouterModule, CommonModule, ReactiveFormsModule],
   templateUrl: './add-contact.html',
   styleUrl: './add-contact.scss'
 })
 export class AddContact {
+   contactForm: FormGroup;
+   isOverlayOpen = false;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [
+        Validators.required,
+        Validators.pattern(/^[A-ZÄÖÜa-zäöüß]+(?:\s[A-ZÄÖÜa-zäöüß]+)+$/)
+      ]],
+      email: ['', [
+        Validators.required,
+        Validators.email,
+        Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+      ]],
+      phone: ['', [
+        Validators.required,
+        Validators.pattern(/^\+?[0-9\s\-]{7,15}$/)
+      ]]
+    });
+  }
+
+   onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      alert('Kontakt erfolgreich erstellt!');
+      this.contactForm.reset();
+      this.closeOverlay();
+    }
+  }
+
+  closeOverlay() {
+    this.isOverlayOpen = false;
+  }
+
+  openOverlay() {
+    this.isOverlayOpen = true;
+  }
 
 }
+
