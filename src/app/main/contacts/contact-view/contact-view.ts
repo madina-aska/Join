8fb362c0ba639e3ad356a4715372a6443051ddbf
Madina Askar Zada <52121682+madina-aska@users.noreply.github.com>
2025-09-23 09:59:59 +0200

    Contact section - Back button implementation for mobile
    * fix(contact-view): adjust mobile layout and add back arrow button/image
    
    * duplicate arrow-left icon removed
    
    * aspect ratio added for the initials
    
    ---------
    
    Co-authored-by: CalvinH-Dev <hanisch.calvin@gmail.com>
    Co-authored-by: Falko Katzer <162439151+hcplayer1988@users.noreply.github.com>

--- src/app/main/contacts/contact-view/contact-view.ts
+++ src/app/main/contacts/contact-view/contact-view.ts
@@ -1,6 +1,6 @@
 import { Component, inject, input, OnChanges } from "@angular/core";
 import { Firestore } from "@angular/fire/firestore";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { ContactService } from "app/core/services/contact-service";
 
 @Component({
@@ -12,11 +12,16 @@ export class ContactView implements OnChanges {
 	firestore = inject(Firestore);
 	contactService = inject(ContactService);
 	route = inject(ActivatedRoute);
+	router = inject(Router);
 	id = input<string>("");
 
 	ngOnChanges() {
 		if (this.id()) {
 			this.contactService.getDocumentById(this.id());
 		}
+	}
+
+	goBack() {
+		this.router.navigate(["/contacts"]);
 	}
 }
