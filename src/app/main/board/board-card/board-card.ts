f485288ba0fe384e87fd7ae4f029b9cb274805fd
Calvin H <hanisch.calvin@gmail.com>
2025-09-26 16:04:41 +0200

    feat: implement task and board components with corresponding templates and styles (#52)

---
+++ src/app/main/board/board-card/board-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-card',
+  imports: [],
+  templateUrl: './board-card.html',
+  styleUrl: './board-card.scss'
+})
+export class BoardCard {
+
+}
---
+++ src/app/main/board/board-view/board-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-view',
+  imports: [],
+  templateUrl: './board-view.html',
+  styleUrl: './board-view.scss'
+})
+export class BoardView {
+
+}
