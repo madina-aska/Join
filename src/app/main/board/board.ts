faeb2715ffb70973d230e62b9e1bff08158dc195
Falko Katzer <162439151+hcplayer1988@users.noreply.github.com>
2025-09-30 10:05:22 +0200

    Task View - mobile & desktop design finished (#57)
    * start with task view design
    
    * assignee finished assignee section
    
    * finished subtask secction
    
    * open & close taask-view overlay
    
    The Task button is a temporary component to test the opening and closing of the overlay.
    
    * desktop design finished

--- src/app/main/board/board.ts
+++ src/app/main/board/board.ts
@@ -1,9 +1,21 @@
 import { Component } from "@angular/core";
+import { TaskView } from "./task-view/task-view";
 
 @Component({
 	selector: "app-board",
-	imports: [],
+	imports: [TaskView],
 	templateUrl: "./board.html",
 	styleUrl: "./board.scss",
 })
-export class Board {}
+export class Board {
+   isTaskViewOpen = false;
+
+  openTaskView() {
+    this.isTaskViewOpen = true;
+  }
+
+  closeTaskView() {
+    this.isTaskViewOpen = false;
+  }
+
+}
--- src/app/main/board/task-view/task-view.ts
+++ src/app/main/board/task-view/task-view.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Button } from '@shared/components/button/button';
 
 @Component({
   selector: 'app-task-view',
-  imports: [],
+  imports: [Button, CommonModule],
   templateUrl: './task-view.html',
   styleUrl: './task-view.scss'
 })
 export class TaskView {
 
+  /** Controls overlay visibility state */
+	isOverlayOpen = false;
+
+  /** Emitted when the task-view overlay is closed */
+  @Output() closed = new EventEmitter<void>();
+
+  /** Emitted when the task-view overlay is opened */
+	@Output() open = new EventEmitter<void>();
+
+  closeOverlay() {
+		this.closed.emit();
+	}
+
+  openOverlay() {
+		this.open.emit();
+	}
+
 }
+
