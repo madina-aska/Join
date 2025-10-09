e54fc1f5c82f1a37ab6294f40b8300055c2a3c95
Calvin H <hanisch.calvin@gmail.com>
2025-10-09 10:40:46 +0200

    feat: implement add task overlay functionality in Board component (#91)
    * feat: implement add task overlay functionality in Board component
    
    * style: enhance overlay styling with backdrop blur and container shadow

--- src/app/main/board/board.ts
+++ src/app/main/board/board.ts
@@ -1,17 +1,21 @@
+import { CdkAutofill } from "@angular/cdk/text-field";
 import { CommonModule, ViewportScroller } from "@angular/common";
 import { Component, inject } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { AddTask } from "@main/add-task/add-task";
+import { AddTaskForm } from "@main/add-task/add-task-form/add-task-form";
 import { BoardView } from "./board-view/board-view";
-
 
 @Component({
 	selector: "app-board",
-	imports: [CommonModule, BoardView],
+	imports: [CommonModule, BoardView, AddTask, AddTaskForm, CdkAutofill],
 	templateUrl: "./board.html",
 	styleUrl: "./board.scss",
 })
 export class Board {
 	isTaskViewOpen = false;
+	addTaskOverlayOpen = false;
+
 	openTaskView() {
 		this.isTaskViewOpen = true;
 	}
@@ -19,4 +23,12 @@ export class Board {
 	vps = inject(ViewportScroller);
 	route = inject(ActivatedRoute);
 	router = inject(Router);
+
+	onAddTaskClicked() {
+		this.addTaskOverlayOpen = true;
+	}
+
+	onCloseAddTask() {
+		this.addTaskOverlayOpen = false;
+	}
 }
