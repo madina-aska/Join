f68cdea92b1d72661979cfc046a4ff10ad6d5441
Falko Katzer <162439151+hcplayer1988@users.noreply.github.com>
2025-10-14 21:10:02 +0200

    Log in window (#111)
    * add Login Window
    
    * login window mobile is finished
    
    * icons to input & desktop beginn

--- src/app/main/auth/login/login.ts
+++ src/app/main/auth/login/login.ts
@@ -1,10 +1,14 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { RouterLink } from "@angular/router";
 
 @Component({
 	selector: "app-login",
-	imports: [RouterLink],
+	imports: [RouterLink, FormsModule],
 	templateUrl: "./login.html",
 	styleUrl: "./login.scss",
 })
-export class Login {}
+export class Login {
+	email: string = "";
+	password: string = "";
+}
