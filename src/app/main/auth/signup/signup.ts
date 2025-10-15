2b6aed473c41f1252b53d3f93e321f72e9755cc3
Falko Katzer <162439151+hcplayer1988@users.noreply.github.com>
2025-10-15 11:32:50 +0200

    design signup (#116)

--- src/app/main/auth/signup/signup.ts
+++ src/app/main/auth/signup/signup.ts
@@ -1,9 +1,35 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
 	selector: "app-signup",
-	imports: [],
+	imports: [RouterLink, FormsModule],
 	templateUrl: "./signup.html",
 	styleUrl: "./signup.scss",
 })
-export class Signup {}
+export class Signup {
+	name: string = "";
+	email: string = "";
+	password: string = "";
+	confirmPassword: string = "";
+	acceptedPolicy: boolean = false;
+
+	constructor(private router: Router) {}
+
+	isFormValid(): boolean {
+		return (
+			this.name.trim().length > 2 &&
+			this.email.trim().length > 4 &&
+			this.password.length > 8 &&
+			this.confirmPassword.length > 8 &&
+			this.acceptedPolicy
+		);
+	}
+
+	navigateToSummary() {
+		if (this.acceptedPolicy) {
+			this.router.navigate(["/summary"]);
+		}
+	}
+}
