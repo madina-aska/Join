980d85356031971c952036f088015c91ac108b5a
Calvin H <hanisch.calvin@gmail.com>
2025-10-21 11:37:22 +0200

    jsdocs for base layout (#160)

--- src/app/layouts/base-layout/base-layout.ts
+++ src/app/layouts/base-layout/base-layout.ts
@@ -4,6 +4,10 @@ import { AuthService } from "@core/services/auth-service";
 import { Footer } from "@shared/footer/footer";
 import { Header } from "@shared/header/header";
 
+/**
+ * Represents the base layout of the application, including the header, footer,
+ * and dynamic content rendered via the router outlet.
+ */
 @Component({
 	selector: "app-base-layout",
 	imports: [Header, RouterOutlet, Footer],
@@ -11,9 +15,18 @@ import { Header } from "@shared/header/header";
 	styleUrl: "./base-layout.scss",
 })
 export class BaseLayout {
-	private authService = inject(AuthService);
+	/**
+	 * Indicates whether the user is currently logged in.
+	 * This value is updated based on the authentication status.
+	 */
 	loggedIn = false;
 
+	private authService = inject(AuthService);
+
+	/**
+	 * Subscribes to the authentication service to update the `loggedIn` status
+	 * whenever the authentication state changes.
+	 */
 	constructor() {
 		this.authService.isLoggedIn$.subscribe((loggedIn) => {
 			this.loggedIn = loggedIn;
