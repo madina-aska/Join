1f4d872526c6041ff43216049db1e275b8c5bf7d
Calvin H <hanisch.calvin@gmail.com>
2025-10-21 11:34:23 +0200

    jsdocs for assigned-list component (#157)

--- src/app/shared/assigned-list/assigned-list.ts
+++ src/app/shared/assigned-list/assigned-list.ts
@@ -3,6 +3,12 @@ import { Component, input } from "@angular/core";
 import { Contact } from "@core/interfaces/contact";
 import { ProfilePicture } from "@shared/components/profile-picture/profile-picture";
 
+/**
+ * A component that renders a list of assigned contacts.
+ *
+ * @remarks
+ * Expects an array of contacts via `assigned`, and a size option via `size`.
+ */
 @Component({
 	selector: "app-assigned-list",
 	imports: [ProfilePicture, SlicePipe],
@@ -10,6 +16,13 @@ import { ProfilePicture } from "@shared/components/profile-picture/profile-pictu
 	styleUrl: "./assigned-list.scss",
 })
 export class AssignedList {
+	/**
+	 * The contacts that are assigned and should be shown in the list.
+	 */
 	assigned = input.required<Contact[]>();
+
+	/**
+	 * Defines the font-size of the component (e.g., large, medium or small).
+	 */
 	size = input<"big" | "medium" | "small">("medium");
 }
